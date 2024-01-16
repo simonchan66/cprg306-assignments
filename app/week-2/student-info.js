7a63f32bd61b4bbc9b86821e31567d0a8fef6991
simonchan66 <formul529@gmail.com>
2024-01-16 14:54:19 -0700

    1st commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <main>
+        <div><h1>My name is Sheung Man Chan Simon</h1></div>
+        <div><h1>Click to go to my github repository</h1></div>
+      </main>
+    );
+  }

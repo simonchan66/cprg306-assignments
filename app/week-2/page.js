d9da7759fcf99a9c84ad99359d1ce57a96e5e7e2
simonchan66 <formul529@gmail.com>
2024-01-16 15:06:13 -0700

    new commit week2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+
+import Student_info from './student_info.js';
 export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+       <Student_info />
     </main>
   );
 }
---
+++ app/week-2/student_info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Page() {
+    return (
+      <main>
+        <div><h1>My name is Sheung Man Chan Simon</h1></div>
+        <div><h1><Link href="https://github.com/simonchan66/cprg306-assignments">Click to go to my github repository</Link></h1></div>
+      </main>
+    );
+  }

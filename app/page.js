e8bc0a9d3fea23cef124c3e94ac93bb1455d6fa8
simonchan66 <formul529@gmail.com>
2024-02-05 22:43:16 -0700

    w5

--- app/page.js
+++ app/page.js
@@ -1,11 +1,31 @@
 import Link from "next/link";
+
 export default function Page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <div><h1><Link href="/week-2">Click to go to WEEK-2</Link></h1></div>
-      <div><h1><Link href="/week-3">Click to go to WEEK-3</Link></h1></div>
-      <div><h1><Link href="/week-4">Click to go to WEEK-4</Link></h1></div>
+    <main className="container mx-auto my-10 p-8 bg-white shadow-md rounded-lg">
+      <h1 className="text-3xl font-bold text-center mb-8">CPRG 306: Web Development 2 - Assignments</h1>
+      <div className="flex flex-col items-center space-y-4">
+        <h2 className="text-xl">
+          <Link href="/week-2" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
+            Click to go to WEEK-2
+          </Link>
+        </h2>
+        <h2 className="text-xl">
+          <Link href="/week-3" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
+            Click to go to WEEK-3
+          </Link>
+        </h2>
+        <h2 className="text-xl" >
+          <Link href="/week-4" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
+           Click to go to WEEK-4
+          </Link>
+        </h2>
+        <h2 className="text-xl" >
+          <Link href="/week-5" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
+           Click to go to WEEK-5
+          </Link>
+        </h2>
+      </div>
     </main>
   );
 }
---
+++ app/week-5/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>WEEK5 - Here is my shopping List</h1>
+      <div>
+        <ItemList />
+      </div>
+      </main>
+  );
+}
